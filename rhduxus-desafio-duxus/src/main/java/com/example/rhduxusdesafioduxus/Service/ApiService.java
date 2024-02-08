e26d80abfcf8881cc3556b92b68b8622be398b95
jennifer cristine <107707471+jennifer-cristine12@users.noreply.github.com>
2024-02-07 23:02:36 -0300

    Add files via upload

--- rhduxus-desafio-duxus/src/main/java/com/example/rhduxusdesafioduxus/Service/ApiService.java
+++ rhduxus-desafio-duxus/src/main/java/com/example/rhduxusdesafioduxus/Service/ApiService.java
@@ -1,10 +1,13 @@
-package com.example.rhduxusdesafioduxus.Service;
+package br.com.duxusdesafio.service;
 
 import br.com.duxusdesafio.model.Integrante;
 import br.com.duxusdesafio.model.Time;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,14 +19,24 @@ import java.util.Map;
  */
 @Service
 public class ApiService {
+  
 
     /**
      * Vai retornar uma lista com os nomes dos integrantes do time daquela data
      */
     public Time timeDaData(LocalDate data, List<Time> todosOsTimes){
         // TODO Implementar método seguindo as instruções!
+        Map<LocalDate, List<String>> map = new HashMap<>();
+        for (Time t :  todosOsTimes){
+           
+        }
+        //pegar os valores da data e os times
+        //usar o map para pegar todos os times de uma só vez (mas primeiro aprender a usar o map)
+        // percorrer a lista de todos os times e verificar quais times possuem a mesma data
+        // usar condicinal para retornar apenas os times correspondentes
         return null;
     }
+   
 
     /**
      * Vai retornar o integrante que tiver presente na maior quantidade de times
